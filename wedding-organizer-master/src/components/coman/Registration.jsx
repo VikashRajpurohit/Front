72d9d2a4eca6b7a08676ca82c023c7ddf4cc830a
ParthDesai091 <98940174+ParthDesai091@users.noreply.github.com>
2022-04-08 15:37:36 +0530

    08/04/22-3-37

--- wedding-organizer-master/src/components/coman/Registration.jsx
+++ wedding-organizer-master/src/components/coman/Registration.jsx
@@ -8,17 +8,6 @@ const Registration = () => {
     <form className="form-detail" action="#" method="post" id="myform">
       <div className="form-left">
         <h2>General Infomation</h2>
-        <div className="form-row">
-          <select name="title">
-            <option className="option" value="title">Title</option>
-            <option className="option" value="businessman">Businessman</option>
-            <option className="option" value="reporter">Reporter</option>
-            <option className="option" value="secretary">Secretary</option>
-          </select>
-          <span className="select-btn">
-            <i className="zmdi zmdi-chevron-down" />
-          </span>
-        </div>
         <div className="form-group">
           <div className="form-row form-row-1">
             <input type="text" name="first_name" id="first_name" className="input-text" placeholder="First Name" required />
@@ -27,83 +16,73 @@ const Registration = () => {
             <input type="text" name="last_name" id="last_name" className="input-text" placeholder="Last Name" required />
           </div>
         </div>
+        <div className="form-group">
+          <div className="form-row form-row-1">
+            <input type="email" name="email" id="email" className="input-text" placeholder="abc@gmail.com" required />
+          </div>
+          <div className="form-row form-row-2">
+            <input type="date" name="dob" id="dob" className="input-text" placeholder="25/10/2000" required />
+          </div>
+        </div>
+        <h2>Address</h2>
+        <div className="form-group">
+          <div className="form-row form-row-1">
+            <input type="text" name="Block-no" id="block-no" className="input-text" placeholder="a/21" required />
+          </div>
+          <div className="form-row form-row-2">
+            <input type="text" name="stret" id="stret" className="input-text" placeholder="Line" required />
+          </div>
+        </div>
         <div className="form-row">
-          <select name="position">
-            <option value="position">Position</option>
-            <option value="director">Director</option>
-            <option value="manager">Manager</option>
-            <option value="employee">Employee</option>
+          <input type="number" name="Pincode" className="company" id="Pincode" placeholder="394230" required />
+        </div>
+        
+        <div className="form-row">
+          <select name="State">
+            <option value="State">State</option>
+            <option value="Gujrat">Gujrat</option>
+            <option value="Punjab">Punjab</option>
+            <option value="UP">UP</option>
           </select>
           <span className="select-btn">
             <i className="zmdi zmdi-chevron-down" />
           </span>
         </div>
+
         <div className="form-row">
-          <input type="text" name="company" className="company" id="company" placeholder="Company" required />
+          <select name="City">
+            <option value="City">City</option>
+            <option value="Surat">Surat</option>
+            <option value="Bardoli">Bardoli</option>
+            <option value="Vapi">Vapi</option>
+          </select>
+          <span className="select-btn">
+            <i className="zmdi zmdi-chevron-down" />
+          </span>
         </div>
-        <div className="form-group">
-          <div className="form-row form-row-3">
-            <input type="text" name="business" className="business" id="business" placeholder="Business Arena" required />
-          </div>
-          <div className="form-row form-row-4">
-            <select name="employees">
-              <option value="employees">Employees</option>
-              <option value="trainee">Trainee</option>
-              <option value="colleague">Colleague</option>
-              <option value="associate">Associate</option>
-            </select>
-            <span className="select-btn">
-              <i className="zmdi zmdi-chevron-down" />
-            </span>
-          </div>
-        </div>
+
       </div>
       <div className="form-right">
         <h2>Contact Details</h2>
         <div className="form-row">
-          <input type="text" name="street" className="street" id="street" placeholder="Street + Nr" required />
+          <input type="phone" name="phone" className="phone" id="phone" placeholder="7874418758" required />
         </div>
         <div className="form-row">
-          <input type="text" name="additional" className="additional" id="additional" placeholder="Additional Information" required />
+          <input type="number" name="otp" className="otp" id="otp" placeholder="****-***" required />
         </div>
+        <h2>Authorization</h2>
         <div className="form-group">
           <div className="form-row form-row-1">
-            <input type="text" name="zip" className="zip" id="zip" placeholder="Zip Code" required />
-          </div>
-          <div className="form-row form-row-2">
-            <select name="place">
-              <option value="place">Place</option>
-              <option value="Street">Street</option>
-              <option value="District">District</option>
-              <option value="City">City</option>
-            </select>
-            <span className="select-btn">
-              <i className="zmdi zmdi-chevron-down" />
-            </span>
+            <input type="email" name="username" className="username" id="username" placeholder="user@gmail.com" required />
           </div>
         </div>
         <div className="form-row">
-          <select name="country">
-            <option value="country">Country</option>
-            <option value="Vietnam">Vietnam</option>
-            <option value="Malaysia">Malaysia</option>
-            <option value="India">India</option>
-          </select>
-          <span className="select-btn">
-            <i className="zmdi zmdi-chevron-down" />
-          </span>
-        </div>
-        <div className="form-group">
-          <div className="form-row form-row-1">
-            <input type="text" name="code" className="code" id="code" placeholder="Code +" required />
-          </div>
-          <div className="form-row form-row-2">
-            <input type="text" name="phone" className="phone" id="phone" placeholder="Phone Number" required />
-          </div>
+          <input type="password" name="password" id="password" className="input-text" required  placeholder="*********" />
         </div>
         <div className="form-row">
-          <input type="text" name="your_email" id="your_email" className="input-text" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="Your Email" />
+          <input type="password" name="password" id="password" className="input-text" required  placeholder="*********" />
         </div>
+        
         <div className="form-checkbox">
           <label className="container"><p>I do accept the <a href="#" className="text">Terms and Conditions</a> of your site.</p>
             <input type="checkbox" name="checkbox" />
