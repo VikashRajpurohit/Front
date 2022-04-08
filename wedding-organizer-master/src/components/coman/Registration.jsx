1aaa1d900d900f5c239398193b99ea185f6b7742
ParthDesai091 <98940174+ParthDesai091@users.noreply.github.com>
2022-04-08 14:28:10 +0530

    08/04/22-2-28

--- wedding-organizer-master/src/components/coman/Registration.jsx
+++ wedding-organizer-master/src/components/coman/Registration.jsx
@@ -3,36 +3,118 @@ import React from 'react'
 const Registration = () => {
   return (  
     <>
-<div id="register-popup" className="popup-effect" style={{display:"grid",placeItems:"center",margin:"2em auto",padding:"3em 2em",zIndex:"999",backgroundColor:"white"}}>
-  <div className="popup">
-    <h5 className="modal-title text-uppercase">Register</h5>
-    <div className="lregister-form">
-      <form action="#" method="post" className="px-3 pt-3 pb-0">
-        <div className="form-group">
-          <label htmlFor="recipient-name" className="col-form-label">First Name</label>
-          <input type="text" className="form-control" placeholder name="Name" id="recipient-name2" required />
+<div className="page-content">
+  <div className="form-v10-content">
+    <form className="form-detail" action="#" method="post" id="myform">
+      <div className="form-left">
+        <h2>General Infomation</h2>
+        <div className="form-row">
+          <select name="title">
+            <option className="option" value="title">Title</option>
+            <option className="option" value="businessman">Businessman</option>
+            <option className="option" value="reporter">Reporter</option>
+            <option className="option" value="secretary">Secretary</option>
+          </select>
+          <span className="select-btn">
+            <i className="zmdi zmdi-chevron-down" />
+          </span>
         </div>
         <div className="form-group">
-          <label htmlFor="recipient-name" className="col-form-label">Last Name</label>
-          <input type="text" className="form-control" placeholder name="Name" id="recipient-name3" required />
+          <div className="form-row form-row-1">
+            <input type="text" name="first_name" id="first_name" className="input-text" placeholder="First Name" required />
+          </div>
+          <div className="form-row form-row-2">
+            <input type="text" name="last_name" id="last_name" className="input-text" placeholder="Last Name" required />
+          </div>
+        </div>
+        <div className="form-row">
+          <select name="position">
+            <option value="position">Position</option>
+            <option value="director">Director</option>
+            <option value="manager">Manager</option>
+            <option value="employee">Employee</option>
+          </select>
+          <span className="select-btn">
+            <i className="zmdi zmdi-chevron-down" />
+          </span>
+        </div>
+        <div className="form-row">
+          <input type="text" name="company" className="company" id="company" placeholder="Company" required />
         </div>
         <div className="form-group">
-          <label htmlFor="recipient-name" className="col-form-label">Email id</label>
-          <input type="email" className="form-control" placeholder name="Name" id="recipient-name4" required />
+          <div className="form-row form-row-3">
+            <input type="text" name="business" className="business" id="business" placeholder="Business Arena" required />
+          </div>
+          <div className="form-row form-row-4">
+            <select name="employees">
+              <option value="employees">Employees</option>
+              <option value="trainee">Trainee</option>
+              <option value="colleague">Colleague</option>
+              <option value="associate">Associate</option>
+            </select>
+            <span className="select-btn">
+              <i className="zmdi zmdi-chevron-down" />
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="form-right">
+        <h2>Contact Details</h2>
+        <div className="form-row">
+          <input type="text" name="street" className="street" id="street" placeholder="Street + Nr" required />
+        </div>
+        <div className="form-row">
+          <input type="text" name="additional" className="additional" id="additional" placeholder="Additional Information" required />
         </div>
         <div className="form-group">
-          <label htmlFor="recipient-name" className="col-form-label">Mobile Number</label>
-          <input type="text" className="form-control" placeholder name="Name" id="recipient-name5" required />
+          <div className="form-row form-row-1">
+            <input type="text" name="zip" className="zip" id="zip" placeholder="Zip Code" required />
+          </div>
+          <div className="form-row form-row-2">
+            <select name="place">
+              <option value="place">Place</option>
+              <option value="Street">Street</option>
+              <option value="District">District</option>
+              <option value="City">City</option>
+            </select>
+            <span className="select-btn">
+              <i className="zmdi zmdi-chevron-down" />
+            </span>
+          </div>
+        </div>
+        <div className="form-row">
+          <select name="country">
+            <option value="country">Country</option>
+            <option value="Vietnam">Vietnam</option>
+            <option value="Malaysia">Malaysia</option>
+            <option value="India">India</option>
+          </select>
+          <span className="select-btn">
+            <i className="zmdi zmdi-chevron-down" />
+          </span>
         </div>
         <div className="form-group">
-          <label htmlFor="recipient-name1" className="col-form-label">Password</label>
-          <input type="password" className="form-control" placeholder name="Name" id="recipient-name6" required />
+          <div className="form-row form-row-1">
+            <input type="text" name="code" className="code" id="code" placeholder="Code +" required />
+          </div>
+          <div className="form-row form-row-2">
+            <input type="text" name="phone" className="phone" id="phone" placeholder="Phone Number" required />
+          </div>
         </div>
-        <div className="right-w3l">
-          <input type="submit" className="form-control" defaultValue="Get Started" />
+        <div className="form-row">
+          <input type="text" name="your_email" id="your_email" className="input-text" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}" placeholder="Your Email" />
         </div>
-      </form>
-    </div>
+        <div className="form-checkbox">
+          <label className="container"><p>I do accept the <a href="#" className="text">Terms and Conditions</a> of your site.</p>
+            <input type="checkbox" name="checkbox" />
+            <span className="checkmark" />
+          </label>
+        </div>
+        <div className="form-row-last">
+          <input type="submit" name="register" className="register" defaultValue="Register Badge" />
+        </div>
+      </div>
+    </form>
   </div>
 </div>
 </>
