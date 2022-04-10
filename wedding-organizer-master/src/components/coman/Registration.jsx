4bef2d2e33db355ee10099d7ecb07aab3a0bcec5
VikashRajpurohit <98598930+VikashRajpurohit@users.noreply.github.com>
2022-04-10 20:07:08 +0530

    10/04/2022-08:07

--- wedding-organizer-master/src/components/coman/Registration.jsx
+++ wedding-organizer-master/src/components/coman/Registration.jsx
@@ -1,41 +1,90 @@
-import React from 'react'
+import axios from "axios"
+import React, { useState } from "react"
+
 
 const Registration = () => {
+  axios.defaults.withCredentials=true;
+  
+  const [ fname, setfname ] = useState("")
+  const [ lname, setlname ] = useState("")
+  const [ contact, setcontact ] = useState("")
+  const [ d_o_b, setd_o_b ] = useState("")
+  const [ address_street, setaddresss ] = useState("")
+  const [ address_landmark, setaddressl ] = useState("")
+  const [ address_pincode, setaddressp ] = useState("")
+  const [ password, setpassword ] = useState("")
+  const [ username, setusername ] = useState("")
+  const [ otp, setotp ] = useState("")
+
+
+	
+  let err= "";  
+	async function postName(e) {
+		e.preventDefault()
+		try {
+			await axios.post("http://localhost:5000/api/users", {
+				otp,fname,lname,contact,address_street,address_landmark,address_pincode,d_o_b,password,username} )
+		} catch (error) {
+      err=error;
+			console.error(error)
+      console.log(error.response.data);
+		}
+    if (err.match("")){
+      console.log("PARTH");
+    }
+	}
+
+  function callopt(){
+  axios.defaults.withCredentials=true;
+    const res = axios.post("http://localhost:5000/api/otp", {username});
+    console.log(res);
+  }
+
   return (  
+
     <>
 <div className="page-content">
   <div className="form-v10-content">
-    <form className="form-detail" action="#" method="post" id="myform">
+    <form className="form-detail" action="#" onSubmit={postName} method="post" id="myform">
       <div className="form-left">
         <h2>General Infomation</h2>
         <div className="form-group">
           <div className="form-row form-row-1">
-            <input type="text" name="first_name" id="first_name" className="input-text" placeholder="First Name" required />
+            <input type="text" name="first_name" id="first_name" className="input-text" placeholder="First Name" 
+                          value={fname} onChange={(e) => setfname(e.target.value)} required />
           </div>
           <div className="form-row form-row-2">
-            <input type="text" name="last_name" id="last_name" className="input-text" placeholder="Last Name" required />
+            <input type="text" name="last_name" id="last_name" className="input-text" placeholder="Last Name" 
+              value={lname} onChange={(e) => setlname(e.target.value)} required />
           </div>
         </div>
         <div className="form-group">
           <div className="form-row form-row-1">
-            <input type="email" name="email" id="email" className="input-text" placeholder="abc@gmail.com" required />
+          <input type="date" name="dob" id="dob" className="input-text"  
+               value={d_o_b} onChange={(e) => setd_o_b(e.target.value)} required />
           </div>
           <div className="form-row form-row-2">
-            <input type="date" name="dob" id="dob" className="input-text" placeholder="25/10/2000" required />
-          </div>
+          <input type="phone" name="phone" className="phone" id="phone" placeholder="Contact.no" 
+                          value={contact} onChange={(e) => setcontact(e.target.value)} required />
         </div>
+        
+         </div>
         <h2>Address</h2>
         <div className="form-group">
           <div className="form-row form-row-1">
-            <input type="text" name="Block-no" id="block-no" className="input-text" placeholder="a/21" required />
+            <input type="text" name="Block-no" id="block-no" className="input-text" placeholder="Block.no/Floor" 
+                            value={address_street} onChange={(e) => setaddresss(e.target.value)} required />
+                            
           </div>
           <div className="form-row form-row-2">
-            <input type="text" name="stret" id="stret" className="input-text" placeholder="Line" required />
+            <input type="text" name="stret" id="stret" className="input-text" placeholder="Street/Landmark" 
+                            value={address_landmark} onChange={(e) => setaddressl(e.target.value)} required />
           </div>
         </div>
         <div className="form-row">
-          <input type="number" name="Pincode" className="company" id="Pincode" placeholder="394230" required />
-        </div>
+          <input type="number" name="Pincode" className="company" id="Pincode" placeholder="Pincode" 
+                          value={address_pincode} onChange={(e) => setaddressp(e.target.value)} required />
+                          </div>
         
         <div className="form-row">
           <select name="State">
@@ -63,24 +112,28 @@ const Registration = () => {
 
       </div>
       <div className="form-right">
-        <h2>Contact Details</h2>
-        <div className="form-row">
-          <input type="phone" name="phone" className="phone" id="phone" placeholder="7874418758" required />
-        </div>
-        <div className="form-row">
-          <input type="number" name="otp" className="otp" id="otp" placeholder="****-***" required />
-        </div>
-        <h2>Authorization</h2>
-        <div className="form-group">
-          <div className="form-row form-row-1">
-            <input type="email" name="username" className="username" id="username" placeholder="user@gmail.com" required />
+        <h2>Authentication Details</h2>
+        <div className="form-row form-row-1"  >
+          <input type="email" name="username"  className="username" id="username" placeholder="Username" 
+                          value={username} onChange={(e) => setusername(e.target.value)} required 
+          />
+          <div className="form-row-last" style={{padding:"unset",margin:"unset",marginLeft:"78%"}}>
+          <input type="button" name="otp" onClick={callopt}className="register" defaultValue="send otp" style={{padding:"unset",margin:"6px 0 0 0 ", width:"100px"}} />
           </div>
+          
+          
+          <input type="text" name="otp"  className="otp" id="otp" placeholder="OTP" 
+                          value={otp} onChange={(e) => setotp(e.target.value)} required 
+          />
+          
         </div>
         <div className="form-row">
-          <input type="password" name="password" id="password" className="input-text" required  placeholder="*********" />
+          <input type="password" name="password" id="password" className="input-text" required  placeholder="Password" />
         </div>
         <div className="form-row">
-          <input type="password" name="password" id="password" className="input-text" required  placeholder="*********" />
+          <input type="password" name="password" id="password" className="input-text" placeholder="Conform Password" 
+                          value={password} onChange={(e) => setpassword(e.target.value)} required />
+          
         </div>
         
         <div className="form-checkbox">
